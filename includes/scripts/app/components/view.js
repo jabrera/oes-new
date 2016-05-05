47ca31f1120d7211c703e86a0dde9767b94d78bc
juvarabrera <juvar.abrera2@gmail.com>
2016-05-05 21:27:46 +0800

    Added view cover.

--- includes/scripts/app/components/view.js
+++ includes/scripts/app/components/view.js
@@ -1,8 +1,26 @@
 View = {
     ready: function() {
         this.el = $(".view");
+		this.whenScroll();
+		this.el.scroll(function() {
+			View.whenScroll();
+		})
         App.log("\t\tView initialized.");
     },
+	whenScroll: function() {
+		setTimeout(function() {
+			$(".view>.cover").each(function() {
+				//console.log("Cover " + $i + ": " + Math.floor($(".view").scrollTop()+60) + " >= " + Math.floor($(this).parent().scrollTop() + $(this).offset().top) + " && " + Math.floor($(".view").scrollTop()+60) + " <= " + Math.floor($(this).parent().scrollTop() + $(this).offset().top + $(this).outerHeight()));
+				if(Math.floor($(".view").scrollTop()+60) >= Math.floor($(this).parent().scrollTop() + $(this).offset().top) && Math.floor($(".view").scrollTop()+60) <= Math.floor($(this).parent().scrollTop() + $(this).offset().top + $(this).outerHeight()) && ActionBar.el.css("background-color") == $(this).css("background-color")) {
+					//console.log(ActionBar.el.css("background-color") + " " + $(this).css("background-color"));
+					ActionBar.el.addClass("flat");
+					return false;
+				} else {
+					ActionBar.el.removeClass("flat");
+				}
+			})
+		}, 20);
+	},
     slide: function(direction) {
         if(direction == "right") {
             this.el.animate({
